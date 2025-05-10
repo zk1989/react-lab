b3b58f1a1f93df2e62a1e61dbdf19db739fe995a
zkocik <zkocik@d17-431-pc-20.ipa.ii.agh.edu.pl>
2025-05-10 15:39:28 +0200

    Add1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [email, setEmail] = useState();
+    const [logged, setLogged] = useState(false)
+
+    function handleChange(event) {
+        setEmail(event.target.value);
+    }
+
+    function handleClick(event) {
+        setLogged(logged => !logged);
+    }
+
+    // let message;
+    // if (email.length < 10) {
+    //     message = <div>Ale masz krótki adres!</div>;
+    // } else if (email.length < 15) {
+    //     message = <div>Twój adres e-mail jest w sam raz.</div>;
+    // } else {
+    //     message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
+    // }
+    //
+    // return (
+    //     <div>
+    //         <h1>System do zapisów na zajęcia</h1>
+    //         <h2>Twój e-mail to {email}.</h2>
+    //         <h3>{message}</h3>
+    //         <input type="text" onChange={handleChange} value={email} />
+    //         <button type="button" onClick={() => alert(email)}>
+    //             Wyświetl mój e-mail w alercie
+    //         </button>
+    //     </div>
+    // );
+
+
+    let display;
+    if (logged === false) {
+        display = <div>
+            <h1>Witaj w systemie zapisow na zajecia</h1>
+            <h2>Zaloguj sie mailem</h2>
+            <input type="text" onChange={handleChange} />
+            <button type="button" onClick={handleClick}>Wchodze</button>
+        </div>
+    } else {
+        display = <div><h3>Witaj {email}</h3>
+            <button onClick={handleClick}>Wyloguj</button></div>
+    }
+
+    return display;
 }
 
 export default App;
